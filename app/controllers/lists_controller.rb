b216d6085ceab4316160e2b58a9d26d54dee5043
rachel toh <racheltohxy@gmail.com>
2021-11-26 17:43:37 +0800

    watch-list done

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -13,13 +13,22 @@ class ListsController < ApplicationController
 
   def create
     @list = List.new(list_params)
-    @list.save
+    if @list.save
+      redirect_to lists_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @list = List.find(params[:id])
+    @list.destroy
     redirect_to lists_path
   end
 
   private
 
   def list_params
-    params.require(:list).permit(:name)
+    params.require(:list).permit(:name, :photo)
   end
 end
